f0651e4889934474362ed53b0c84bcfa520831bd
Susanne Ehmann <susanne.ehmann@schulen.nuernberg.de>
2023-03-18 02:59:00 +0100

    Klasse Rechteck - Konstruktor

--- Rechteck.java
+++ Rechteck.java
@@ -2,4 +2,9 @@ public class Rechteck {
     private int laenge;
     private int breite;
     
+    public Rechteck(int laenge, int breite) {
+        this.laenge = laenge;
+        this.breite = breite;
+    }
+    
 }
